26539e3412c7847a20303e244e2a278f473e1117
u21430854 <u21430854@tuks.co.za>
2022-10-30 21:59:52 +0200

    Added: - Functionality to display orders and pagination

--- u21430854_HW06/Controllers/OrdersController.cs
+++ u21430854_HW06/Controllers/OrdersController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using u21430854_HW06.Models;
+using u21430854_HW06.ViewModels;
+using System.Data;
+using System.Data.Entity;
+using PagedList.Mvc;
+using PagedList;
 
 namespace u21430854_HW06.Controllers
 {
     public class OrdersController : Controller
     {
+        BikeStoresEntities db = new BikeStoresEntities();
+
         // GET: Orders
-        public ActionResult Index()
+        public ActionResult Orders(int? i)
         {
-            return View();
+            db.Configuration.ProxyCreationEnabled = false; //save memory
+            OrdersVM ordVM = new OrdersVM();
+            //I need product name but for some reason, product name only comes when I have a vm with a static products list
+            ordVM.allOrders = db.orders.Include(oo => oo.order_items).OrderBy(oo => oo.order_id).ToPagedList(i ?? 1, 10);
+            OrdersVM.allProducts = db.products.ToList();
+
+            return View(ordVM);
         }
     }
 }
--- u21430854_HW06/ViewModels/OrdersVM.cs
+++ u21430854_HW06/ViewModels/OrdersVM.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using u21430854_HW06.Models;
+using PagedList.Mvc;
+using PagedList;
 
 namespace u21430854_HW06.ViewModels
 {
     public class OrdersVM
     {
+        public IPagedList<order> allOrders { get; set; }
+        public static List<product> allProducts { get; set; }
+
+        //calc grand total per order
+        public decimal CalculateGrandTotal(int orderId)
+        {
+            decimal grandTotal = 0;
+
+            foreach (order_items orditem in allOrders.FirstOrDefault(oo => oo.order_id == orderId).order_items)
+            {
+                grandTotal += orditem.quantity * orditem.list_price;
+            }
+
+            return grandTotal;
+        }
     }
 }
